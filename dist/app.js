fe278f751e5fc36bab1f44b2e4cd2af6ba0b3376
NotMarvle <puffythereall@gmail.com>
2023-02-26 14:47:00 +0100

    idk how to use git yet so ignore this message

---
+++ dist/app.js
@@ -0,0 +1,8 @@
+var color = document.getElementById("color");
+var colorVal = (color.value);
+document.getElementById("out").innerHTML = colorVal;
+function submit() {
+    var color = document.getElementById("color");
+    var colorVal = (color.value);
+    document.getElementById("out").innerHTML = colorVal;
+}
---
+++ src/app.ts
@@ -0,0 +1,8 @@
+var color = document.getElementById("color") as HTMLInputElement;
+var colorVal = (color.value);
+document.getElementById("out").innerHTML = colorVal
+function submit() {
+    var color = document.getElementById("color") as HTMLInputElement;
+    var colorVal = (color.value);
+    document.getElementById("out").innerHTML = colorVal;
+}
